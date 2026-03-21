0f69951e9bb45cf659e659e595151979fbbae074
Ted <teddexter0@gmail.com>
2026-03-21 11:02:16 +0300

    fix: add COOP unsafe-none header for mobile OAuth

--- next.config.js
+++ next.config.js
@@ -2,16 +2,24 @@
 const nextConfig = {
   reactStrictMode: true,
   async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          // Allows Google OAuth popup to communicate back to the page
-          { key: 'Cross-Origin-Opener-Policy', value: 'same-origin-allow-popups' },
-        ],
-      },
-    ];
-  },
-};
+  return [
+    {
+      source: '/api/:path*',
+      headers: [
+        { key: 'Access-Control-Allow-Credentials', value: 'true' },
+        { key: 'Access-Control-Allow-Origin', value: '*' },
+        { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+        { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+      ]
+    },
+    {
+      source: '/(.*)',
+      headers: [
+        { key: 'Cross-Origin-Opener-Policy', value: 'unsafe-none' },
+      ],
+    }
+  ];
+}
+}
 
 module.exports = nextConfig;
